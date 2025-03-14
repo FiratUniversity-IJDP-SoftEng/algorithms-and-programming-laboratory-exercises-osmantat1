b74afb48314e5e83123c664481813919957f1933
osmantat1 <153751193+osmantat1@users.noreply.github.com>
2025-03-14 22:26:31 +0300

    Update ex001.py

--- Exercises/ex001.py
+++ Exercises/ex001.py
@@ -1,2 +1,3 @@
-# Your Student ID:
-# Your Name and Surname:
+# Your Student ID:230543002
+# Your Name and Surname:Osman Tat
+print("""Twinkle, twinkle, little star,\n\tHow I wonder what you are! \n\t\tUp above the world so high,\n\t\tLike a diamond in the sky. \nTwinkle, twinkle, little star, \n\tHow I wonder what you are""")
