deac29a2b269d79ee686469807eafee895ab4724
osmantat1 <153751193+osmantat1@users.noreply.github.com>
2025-03-14 22:34:00 +0300

    Update ex008.py

--- Exercises/ex008.py
+++ Exercises/ex008.py
@@ -1,2 +1,6 @@
-# Your Student ID:
-# Your Name and Surname:
+# Your Student ID:230543002
+# Your Name and Surname:Osman Tat
+n = int(input("Enter a number: "))
+for i in range(1, n * 2, 2):
+    print(" " * ((n * 2 - 1 - i) // 2) + "*" * i)
+
