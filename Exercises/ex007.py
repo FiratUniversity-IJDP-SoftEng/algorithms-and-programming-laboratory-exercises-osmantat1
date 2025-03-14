921c41951da132b30bfae47ff192b4577904252a
osmantat1 <153751193+osmantat1@users.noreply.github.com>
2025-03-14 22:33:19 +0300

    Update ex007.py

--- Exercises/ex007.py
+++ Exercises/ex007.py
@@ -1,2 +1,7 @@
-# Your Student ID:
-# Your Name and Surname:
+# Your Student ID:230543002
+# Your Name and Surname:Osman Tat
+text = input("Enter a string: ")
+char_count = {char: text.count(char) for char in sorted(set(text))}
+print("Character frequencies:")
+for char, count in char_count.items():
+    print(f"{char}: {count}"
