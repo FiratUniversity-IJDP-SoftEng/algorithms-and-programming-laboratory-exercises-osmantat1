28c02ed231c9b8da19b0823b093ce468ffcf4279
osmantat1 <153751193+osmantat1@users.noreply.github.com>
2025-03-14 22:27:47 +0300

    Update ex002.py

--- Exercises/ex002.py
+++ Exercises/ex002.py
@@ -1,2 +1,6 @@
-# Your Student ID:
-# Your Name and Surname:
+# Your Student ID:230543002
+# Your Name and Surname:Osman Tat
+import sys
+print(f"Python version: {sys.version.split()[0]}")
+print(f"Version info: {sys.version_info}")
+
